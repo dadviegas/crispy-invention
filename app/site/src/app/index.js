3b670c7e6cc4d6c681f81bffedf5be3aeb4ab61c
David Viegas <david.viegas@sky.uk>
2018-11-26 17:54:10 +0000

    theme

--- app/site/src/app/index.js
+++ app/site/src/app/index.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Index = () => {
-  return <div>Hello React!</div>;
+const themes = ['royal', 'solidVault', 'fireWatch', 'servQuick', 'sunrise', 'mirage', 'stellar'];
+class App extends Component {
+  state = {
+    theme: 'stellar',
+    i: 0,
+  }
+themes
+  componentWillMount() {
+    // setInterval(() => {
+    //   console.log(this.state.i, this.state.i % themes.length)
+    //   this.setState({
+    //     theme: themes[this.state.i % themes.length],
+    //     i: this.state.i + 1,
+    //   })
+    // }, 3000)
+  }
+
+  render() {
+    return <section id="theme-background" className={this.state.theme}>Hello React!</section>;
+  }
 };
 
-export default Index;
+export default App;
--- app/site/src/index.js
+++ app/site/src/index.js
@@ -4,7 +4,7 @@ import App from './app';
 
 import '../styles/core.scss';
 
-const render = () => ReactDOM.render(<App />, document.getElementById("index"));
+const render = () => ReactDOM.render(<App />, document.getElementById("root"));
 
 module.hot && module.hot.accept('./app', () => setTimeout(render));
 
