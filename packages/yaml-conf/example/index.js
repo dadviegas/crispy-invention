ce1e2593639ea1a518f19e961118e84f8e8c1aea
David Viegas <david.viegas@sky.uk>
2018-12-15 11:27:31 +0000

    rafactor

--- packages/yaml-conf/example/index.js
+++ packages/yaml-conf/example/index.js
@@ -1,3 +1,17 @@
-const config = require('../lib').default;
+const config = require('../src').default;
 
-config('./config.yml');
+var variables = [
+  { key: 'test1', value: '_test1_'},
+  { key: 'test2', value: '_test2_'},
+];
+
+const a = config('./example/config.yml', 'test', variables);
+const b = config('./example/config.yml', 'development', variables);
+const c = config('./example/config.yml', 'production', variables);
+const d = config('./example/config.yml', 'production');
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log(d);
--- packages/yaml-conf/src/index.js
+++ packages/yaml-conf/src/index.js
@@ -2,8 +2,15 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 import deepmerge from 'deepmerge';
 
-export default (yamlFile, environment = 'production') => {
-  var result = yaml.safeLoad(fs.readFileSync(yamlFile, 'utf8'));
+export default (yamlFile, environment = 'production', transformOptions = []) => {
+  const rawYaml = fs.readFileSync(yamlFile, 'utf8');
+  let transformedYmal = rawYaml;
+
+  transformOptions.forEach((pair) => {
+    transformedYmal = transformedYmal.replace(pair.key, pair.value);
+  });
+
+  var result = yaml.safeLoad(transformedYmal);
 
   const defaultData = result['default'] || {};
   const environmentData = result[environment] || {};
